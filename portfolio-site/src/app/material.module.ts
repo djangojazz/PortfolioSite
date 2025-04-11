9e0942c6ca1d39881133920c4b00d6c02a0faa2f
djangojazz <brettmorin2@gmail.com>
2025-04-10 21:37:04 -0700

    Update navigation styling: increase width to 17%, remove rounded corners, and fix Material theme configuration

--- portfolio-site/src/app/material.module.ts
+++ portfolio-site/src/app/material.module.ts
@@ -3,13 +3,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class MaterialModule { } 
