f39e7248bbbdca02a348673e0e16131f13c2fcea
djangojazz <brettmorin2@gmail.com>
2025-05-02 16:24:11 -0700

    Added contact section

--- portfolio-site/src/app/app.routes.ts
+++ portfolio-site/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { JobsComponent } from './components/jobs/jobs.component';
 import { CertificationsComponent } from './components/certifications/certifications.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutMeComponent },
   { path: 'jobs', component: JobsComponent },
   { path: 'certifications', component: CertificationsComponent },
+  { path: 'contact', component: ContactComponent},
   { path: '**', redirectTo: '/about' }
 ];
